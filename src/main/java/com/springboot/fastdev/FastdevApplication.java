18acc9ef0e1d9bbb4c075f4efd6f5532b392ff39
Kelvin Hey <kelvinhey@outlook.com>
2025-01-07 19:27:14 -0300

    feat: adiciona arquivos inicias do projeto

---
+++ src/main/java/com/springboot/fastdev/FastdevApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fastdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/fastdev/FastdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fastdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
