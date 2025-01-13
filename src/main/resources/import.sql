e73a1ee608b84ff3cf13ee2454bb3c5c7674d4b5
Kelvin Hey <kelvinhey@outlook.com>
2025-01-13 18:58:57 -0300

    feat: adiciona arquivo inicial de sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuario (nome, email) VALUES ('João Silva', 'joao.silva@email.com');
+INSERT INTO usuario (nome, email) VALUES ('Maria Silva', 'maria.silva@email.com');
+INSERT INTO usuario (nome, email) VALUES ('Pedro Silva', 'carlos.silva@email.com');
