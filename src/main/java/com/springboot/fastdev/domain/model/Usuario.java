a236b0828a1734b138889b68187762403cbc6817
Kelvin Hey <kelvinhey@outlook.com>
2025-01-27 07:40:11 -0300

    feat: adiciona classe model de Usuario

---
+++ src/main/java/com/springboot/fastdev/domain/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.springboot.fastdev.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nome;
+    
+    @Column(nullable = false)
+    private String email;
+}
