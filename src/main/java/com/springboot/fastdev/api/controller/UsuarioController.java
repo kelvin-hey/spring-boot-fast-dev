6c97766b19771522376d4fbec6c6f1094962e439
Kelvin Hey <kelvinhey@outlook.com>
2025-02-23 18:08:28 -0300

    feat: implementa o cadastro de usuarios

--- src/main/java/com/springboot/fastdev/api/controller/UsuarioController.java
+++ src/main/java/com/springboot/fastdev/api/controller/UsuarioController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springboot.fastdev.domain.model.Usuario;
+import com.springboot.fastdev.domain.repository.UsuarioRepository;
 import com.springboot.fastdev.domain.service.UsuarioService;
 
 @Controller
@@ -27,6 +30,14 @@ public class UsuarioController {
     	model.addAttribute("usuarios", listaUsuarios);
     	model.addAttribute("totalUsers", String.valueOf(listaUsuarios.size()));       
     	
+    	model.addAttribute("usuario", new Usuario());
+    	
     	return "usuarios";
+    }
+    
+    @PostMapping
+    public String cadastrarUsuario(@ModelAttribute Usuario usuario) {
+    	usuarioService.salvar(usuario);
+    	return "redirect:/usuarios";
     }
 }
